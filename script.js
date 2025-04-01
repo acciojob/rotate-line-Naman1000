3cf057c99a4564860e76d032fe20e7ced3399799
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-01 20:09:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let move = document.getElementById("line");
+let angle = 0;
+
+setInterval(()=>{
+	angle+=2;
+	move.style.transform= `rotate(${angle}deg)`
+}, 20)
+
+
